33378bdfd2163f36bd287607dfef4648581bce83
Denisse Medina Flores <denissemedinaflores@gmail.com>
2023-08-12 23:11:06 -0400

    Sprint Complete

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import AppFunctional from "./AppFunctional"
+
+
+test("renders without errors", () => {
+  render(<AppFunctional />);
+
+});
+
+test("When app mounts, coordinates exists on the screen", async () => {
+  render(<AppFunctional />);
+  const header = screen.getByText("Coordinates (2, 2)");
+  expect(header).toBeInTheDocument;
+});
